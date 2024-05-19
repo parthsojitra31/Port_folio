edead4b9916a8a9599dd0bfaebd50490092255bd
parth <parth@gmail.com>
2024-05-19 22:16:16 +0530

    commitg

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["Frontend Developer","Data scientist","Backend Developer"],
+    strings: ["Frontend Developer","Data Scientist","Backend Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
